6fac1e98e24ed0d7aaefe169ce509433914cd3a0
shanch <shanch@cn.ibm.com>
2017-08-11 10:09:09 +0800

    url issue

--- src/app/pages/product/components/selection/selection.service.ts
+++ src/app/pages/product/components/selection/selection.service.ts
@@ -1,3 +1,4 @@
+import { Constants } from './../../../../../constants';
 import { Selection } from './../../model/selection';
 import { RestResult } from './../../../../rest-result';
 import { Injectable } from '@angular/core';
@@ -11,20 +12,20 @@ import { InterceptorService } from "ng2-interceptors";
 export class SelectionService {
     constructor(private http: InterceptorService) { }
     load(): Observable<RestResult<Selection[]>> {
-        const url = "http://localhost:8080/collectionGoods";
+        const url = Constants.API_ENDPOINT+"/collectionGoods";
         return this.http.get(url).map(function (res: Response) {
             return res.json() as RestResult<Selection[]>;
         }).catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
     get(id: string): Observable<RestResult<Selection>> {
         console.log(id);
-        const url = "http://localhost:8080/collectionGoods/" + id;
+        const url = Constants.API_ENDPOINT+"/collectionGoods/" + id;
         return this.http.get(url).map(function (res: Response) {
             return res.json() as RestResult<Selection>;
         }).catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
     save(product:Selection):Observable<RestResult<Selection>>{
-         const url = "http://localhost:8080/collectionGoods/";
+         const url = Constants.API_ENDPOINT+"/collectionGoods/";
         return this.http.post(url,product).map(function (res: Response) {
             return res.json() as RestResult<Selection>;
         }).catch((error: any) => Observable.throw(error.json().error || 'Server error')); 
