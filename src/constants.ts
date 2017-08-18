2a34338a69cb227f41511e60a593f897bb4b24aa
shanch <shanch@cn.ibm.com>
2017-08-18 10:13:43 +0800

    change port

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export class Constants{
-    public static API_ENDPOINT='http://www.huanlemujia.com:8081';
+    public static API_ENDPOINT='http://www.huanlemujia.com:8080';
    // public static API_ENDPOINT='http://localhost:8080';
 }
